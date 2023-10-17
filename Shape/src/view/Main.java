97f8e792876d6e517b2fe319175c61858a9835b8
hoanghkds2 <130424886+hoanghkds2@users.noreply.github.com>
2023-10-17 13:33:36 +0700

    Add files via upload

---
+++ Shape/src/view/Main.java
@@ -0,0 +1,10 @@
+package view;
+
+import controller.ShapeCalculateController;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new ShapeCalculateController().run();
+    }
+}
